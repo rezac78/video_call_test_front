df370e4e59704476d1efe8516e48bcf89c610382
reza.dalvand78@gmail.com <reza.dalvand78@gmail.com>
2024-08-23 20:08:07 +0330

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './index.css'
 import { RoomProvider } from './context/RoomContext.jsx'
 import { Home } from './page/Home.jsx'
 import { Room } from './page/Room.jsx'
-import {Dashboard} from "./page/Dashboard";
+import {Dashboard} from "./page/Dashboard.jsx";
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
