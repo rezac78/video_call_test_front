b2a109ad6bef0583e297c51ab178da77006e1af6
reza <reza.dalvand78@gmail.com>
2024-08-26 16:57:34 +0330

    ok

--- src/page/Room.jsx
+++ src/page/Room.jsx
@@ -1,3 +1,4 @@
+
 import { useParams } from "react-router-dom";
 import { RoomContext } from "../context/RoomContext";
 import { useContext, useEffect, useState } from "react";
@@ -9,7 +10,7 @@ import { SoundTest } from "../components/soundTest";
 export const Room = () => {
   const { id } = useParams();
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const { ws, me, stream } = useContext(RoomContext);
+  const { ws, me, stream, peers } = useContext(RoomContext);
 
   useEffect(() => {
     if (me) {
@@ -21,41 +22,10 @@ export const Room = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
 
-  const peers = {
-    "af5e9919-7cf": {
-      stream: {},
-    },
-    "c63f6cc8-7af": {
-      stream: {},
-    },
-    "8dc31733-169": {
-      stream: {},
-    },
-  };
-
   return (
-    <div className="h-screen overflow-hidden flex flex-col bg-[#202124]">
+    <div className="min-h-screen flex flex-col bg-[#202124] relative">
       <SoundTest />
-      {/* <div className="flex-grow mx-auto h-full overflow-hidden bg-red-400 grid grid-cols-5 grid-rows-2 p-9 gap-6">
-        <div className="w-full h-full col-span-3 row-span-2 bg-yellow-400 ">
-          <VideoPlayer className={"w-full"} stream={stream} />
-        </div>
-        <div className="col-span-2 col-start-4 row-start-1 bg-blue-400 aspect-square">
-          2
-        </div>
-        <div className="col-span-2 col-start-4 row-start-2 bg-pink-600 aspect-square">
-          3
-        </div>
-      </div> */}
-
-      <div className="">
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-
-
-      {/* <div
+      <div
         className={`grid grid-cols-1 ${
           Object.keys(peers).length === 0 ? "lg:grid-cols-2" : "lg:grid-cols-4"
         }  gap-4 flex-1 p-4`}
@@ -72,7 +42,7 @@ export const Room = () => {
             </div>
           ))}
         </div>
-      </div> */}
+      </div>
       <SideBar openModal={isSidebarOpen} handleButton={toggleSidebar} />
       <Footer handleButton={toggleSidebar} IdUsers={id} />
     </div>
